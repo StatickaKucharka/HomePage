4e236fce64eef205895e0be0c0f1ec7f84156e5e
GulityCrown <cupalukas2@gmail.com>
2023-02-14 19:56:59 +0100

    začiatky:))

--- HomePage/MainWindow.xaml.cs
+++ HomePage/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomePage.UI.ReceptAVsetkoOkolo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,38 @@ namespace HomePage
         public MainWindow()
         {
             InitializeComponent();
+            var recipes = GetRecipes();
+            if(recipes.Count > 0)
+            {
+                ListViewRecipes.ItemsSource = recipes;
+            }
         }
+        private List<Recipe> GetRecipes()
+        {
+            return new List<Recipe>()
+            {
+                new Recipe("Recipe 1", "Obrazky/recepty/recept1.jpg"),
+                new Recipe("Recipe 2", "Obrazky/recepty/recept2.jpg"),
+                new Recipe("Recipe 3", "Obrazky/recepty/recept3.jpg"),
+                new Recipe("Recipe 4", "Obrazky/recepty/recept4.jpg"),
+                new Recipe("Recipe 5", "Obrazky/recepty/recept5.jpg"),
+                new Recipe("Recipe 6", "Obrazky/recepty/recept6.jpg"),
+                new Recipe("Recipe 7", "Obrazky/recepty/recept7.jpg"),
+                new Recipe("Recipe 8", "Obrazky/recepty/recept8.jpg"),
+                new Recipe("Recipe 9", "Obrazky/recepty/recept9.jpg"),
+                new Recipe("Recipe 10", "Obrazky/recepty/recept10.jpg"),
+                new Recipe("Recipe 11", "Obrazky/recepty/recept11.jpg"),
+                new Recipe("Recipe 12", "Obrazky/recepty/recept12.jpg"),
+                new Recipe("Recipe 13", "Obrazky/recepty/recept13.jpg"),
+
+            };
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Receptik receptVole = new Receptik();
+            
+        }
+
     }
 }
---
+++ HomePage/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePage
+{
+    internal class Recipe
+    {
+
+
+
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public string Image { get; set; }
+        public Recipe(string name, string image)
+        {
+            Name = name;
+            Image = image;
+        }
+    }
+}
