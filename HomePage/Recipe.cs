77df519c4d9331857e517679f752152b7144016b
GulityCrown <cupalukas2@gmail.com>
2023-03-06 21:12:10 +0100

    posledná verzia pred demom

--- HomePage/Recipe.cs
+++ HomePage/Recipe.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Animation;
 
 namespace HomePage
 {
     public class Recipe
     {
 
+        private string name;
+        private string image; 
 
+       
 
-        public string Name { get; set; }
-        public double Value { get; set; }
-        public string Image { get; set; }
+        public string Name { get { return this.name; } set { this.name = value; } }
+        
+        public string Image { get { return this.image; } set { this.image = value; } }
+        public Recipe() { }
         public Recipe(string name, string image)
         {
-            Name = name;
-            Image = image;
+            this.name = name;
+            this.image = image;
         }
     }
 }
--- HomePage/strany/Page1.xaml.cs
+++ HomePage/strany/Page1.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Annotations;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -21,28 +23,36 @@ namespace HomePage.strany
     /// </summary>
     public partial class Page1 : Page
     {
+        LandingPage ach = new LandingPage();
+
         public Page1()
         {
             InitializeComponent();
-        }
-
-        private void CestaKObrazku_TextChanged(object sender, TextChangedEventArgs e)
-        {
+            
 
         }
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
             
-            LandingPage achh = new LandingPage();
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to add recipe?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if(result == MessageBoxResult.Yes) { 
-                
-                achh.AddRecipeToList(new Recipe(TBXNadpis.Text, CestaKObrazku.Text));
+            Recipe recept = new Recipe(TBXNadpis.Text, CestaKObrazku.Text);
+            this.DataContext = recept;
+            ach.AddRecipe(recept);
+            
+            NavigateToPage2();
+
+        }
+       
+
+        private void NavigateToPage2()
+        {
+            if (ach == null)
+            {
+                ach = new LandingPage();
             }
-            
-            
-            
+
+            NavigationService.Navigate(ach);
         }
     }
 }
+
